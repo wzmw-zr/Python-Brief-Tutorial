ee3066372e7f4d207052355d9cbb50b97d9e6392
wzmw-zr <1767153298@qq.com>
2020-11-17 00:51:13 +0800

    2020.11.17

---
+++ 5.流程控制语句/if_elif_else_example.py
@@ -0,0 +1,7 @@
+x = int(input("Please input a interger : "))
+if x < 0:
+    print("negative number")
+elif x <= 100:
+    print("non-negative number not larger than 100")
+else:
+    print("number larger than 100")
